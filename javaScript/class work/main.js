80e2afd96d77c216463effe1144e229d3d92de28
Odunayo17 <olufemiodunayo096@gmail.com>
2024-01-26 13:56:39 -0800

    tutorial

---
+++ javaScript/class work/main.js
@@ -0,0 +1,117 @@
+let inp = prompt("what's your score")
+
+
+switch (inp) {
+    case "50":
+        alert("Your Grade Is C")
+        break;
+
+    case "51":
+        alert("Your Grade Is C")
+        break;
+
+    case "52":
+        alert("Your Grade Is C")
+        break;
+
+    case "53":
+        alert("Your Grade Is C")
+        break;
+
+    case "54":
+        alert("Your Grade Is C")
+        break;
+
+    case "55":
+        alert("Your Grade Is C")
+        break;
+
+    case "56":
+        alert("Your Grade Is BC")
+        break;
+
+    case "57":
+        alert("Your Grade Is BC")
+        break;
+
+    case "58":
+        alert("Your Grade Is BC")
+        break;
+
+    case "59":
+        alert("Your Grade Is BC")
+        break;
+
+    case "60":
+        alert("Your Grade Is BC")
+        break;
+
+    case "61":
+        alert("Your Grade Is B")
+        break;
+
+    case "62":
+        alert("Your Grade Is B")
+        break;
+
+    case "63":
+        alert("Your Grade Is B")
+        break;
+
+    case "64":
+        alert("Your Grade Is B")
+        break;
+
+    case "65":
+        alert("Your Grade Is B")
+        break;
+
+    case "66":
+        alert("Your Grade Is A")
+        break;
+
+    case "67":
+        alert("Your Grade Is A")
+        break;
+
+    case "688":
+        alert("Your Grade Is A")
+        break;
+
+    case "69":
+        alert("Your Grade Is A")
+        break;
+
+    case "70":
+        alert("Your Grade Is A")
+        break;
+
+    case "75":
+        alert("Your Grade Is AA")
+        break;
+
+    case "76":
+        alert("Your Grade Is AA")
+        break;
+
+    case "77":
+        alert("Your Grade Is AA")
+        break;
+
+    case "78":
+        alert("Your Grade Is AA")
+        break;
+
+    case "79":
+        alert("Your Grade Is AA")
+        break;
+
+    case "80":
+        alert("Your Grade Is AA")
+        break;
+
+    default:
+        alert("not recognized")
+        break;
+}
+
